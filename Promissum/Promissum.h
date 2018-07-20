c267e86def261517430b539b14bb7cf5ca329adb
Mathijs Bernson <mathijsb@q42.nl>
2018-07-20 17:49:10 +0200

    Add Xcode project for Carthage

---
+++ Promissum/Promissum.h
@@ -0,0 +1,19 @@
+//
+//  Promissum.h
+//  Promissum
+//
+//  Created by Mathijs Bernson on 20/07/2018.
+//  Copyright © 2018 Tom Lokhorst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promissum.
+FOUNDATION_EXPORT double PromissumVersionNumber;
+
+//! Project version string for Promissum.
+FOUNDATION_EXPORT const unsigned char PromissumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promissum/PublicHeader.h>
+
+
